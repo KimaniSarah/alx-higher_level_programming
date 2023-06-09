9afe9c875f050e93ea3e36fc1dd996d6482f31e7
KimaniSarah <bibokimani@gmailcom>
2023-06-09 10:11:21 -0700

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python3
 def pow(a, b):
     result = 1
     if b < 0:
         a = 1 / a
         b = -b
-    while b > 0:
-        if b % 2 != 0:
-            result *= a
-        a *= a
-        b >>= 1
+    for _ in range(b):
+        result *= a
     return result
+
