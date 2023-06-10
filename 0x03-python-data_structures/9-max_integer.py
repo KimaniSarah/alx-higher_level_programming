308b99bd29b1f9f613622d4b8621444292b04a57
KimaniSarah <bibokimani@gmailcom>
2023-06-10 10:44:30 -0700

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
+
     max_num = my_list[0]
     else:
         for num in my_list:
