d699fdcea9c8e578293eae6eeeb784418b1b589a
KimaniSarah <bibokimani@gmailcom>
2023-06-11 01:20:19 -0700

    Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = subtract(a, b)
+    elif operator == '*':
+        result = multiply(a, b)
+    elif operator == '/':
+        result = divide(a, b)
+    else:
+        sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
