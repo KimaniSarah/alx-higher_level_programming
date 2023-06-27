757db9954b4f21c993a01c62fbc69eb660839fe3
KimaniSarah <bibokimani@gmailcom>
2023-06-27 02:50:31 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,8 @@
 
 
 class Square:
+    """square class"""
     def __init__(self, size):
+        """initializes a square instance
+        args:size (int): Size of the square """
         self.__size = size
