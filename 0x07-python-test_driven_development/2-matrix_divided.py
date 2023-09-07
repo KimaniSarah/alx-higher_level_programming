851c70835d29c17ee71b8f140fa6ce4b420ffc1c
KimaniSarah <bibokimani@gmailcom>
2023-09-06 19:24:50 -0700

    matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,24 +14,24 @@ def matrix_divided(matrix, div):
 
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists)"
-                " of integers/floats")
+                        " of integers/floats")
 
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists)"
-                " of integers/floats")
+                        " of integers/floats")
 
     for row in matrix:
         for ele in row:
             if not isinstance(ele, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists)"
-                        " of integers/floats")
+                                " of integers/floats")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     first_row_size = len(matrix[0])
     if any(len(row) != first_row_size for row in matrix):
-        raise ValueError("Each row of the matrix must have the same size")
+        raise ValueError("matrix must have each row with the same size")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
