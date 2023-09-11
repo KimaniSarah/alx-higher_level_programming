eadc08b13944dd5f0efe364583c0e0e1ded010f8
KimaniSarah <bibokimani@gmailcom>
2023-09-11 09:18:19 -0700

    full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python3
 """
-a class base geometry
+A class representing a rectangle.
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-    a class with one method
+    A class representing a rectangle.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+
+    Methods:
+        __init__(self, width, height): Initializes a Rectangle instance.
+        area(self): Computes and returns the area of the rectangle.
+        __str__(self): Returns a string representation of the rectangle.
     """
+
     def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance with the given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is not a positive integer.
+        """
         self.__width = width
         self.__height = height
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
 
     def __str__(self):
-        return("[Rectangle] {}/{}".format(self.__width, self.__height))
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: A string in the format "[Rectangle] <width>/<height>"
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
     def area(self):
-        """return the area of the retangle"""
-        result = self.__width * self.__height
-        return result
+        """
+        Computes and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
