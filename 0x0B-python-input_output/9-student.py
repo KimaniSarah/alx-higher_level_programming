e342dc7a217c1c96c837e1b05eba9328d2c1734c
KimaniSarah <bibokimani@gmailcom>
2023-09-12 11:13:25 -0700

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,25 +4,18 @@
 
 class Student:
     """
-    defines student
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-        initializes the instance Student
+    defines student ,initializes the instance Student
         Args:
             first_name:
             last_name:
             age:
-        """
+    """
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        retrieves a dictionary representation of a Student
-        instance (same as 8-class_to_json.py)
-        """
         Student_dict = {
                 "first_name": self.first_name,
                 "last_name": self.last_name,
