06d33e964704a2ef84296d9426b766306fb4324f
KimaniSarah <bibokimani@gmailcom>
2023-09-12 02:16:44 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+""" gives the JSON representation of an object(string)"""
+
+
+def to_json_string(my_obj):
+    """
+    gives a json rep of an object
+    Args:
+        my_obj:the object to be rep in json string
+    Return:
+        char: JSON representation of an object (string):
+    """
+    json_file = json.dumps(my_obj)
+    return json_file
