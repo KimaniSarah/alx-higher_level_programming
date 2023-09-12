cf8625f32ed823e6fe3b63b73b235f9ebed343dd
KimaniSarah <bibokimani@gmailcom>
2023-09-12 05:02:51 -0700

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+try:
+    existing_data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    existing_data = []
+
+new_arguments = sys.argv[1:]
+updated_data = existing_data + new_arguments
+save_to_json_file(updated_data, "add_item.json")
