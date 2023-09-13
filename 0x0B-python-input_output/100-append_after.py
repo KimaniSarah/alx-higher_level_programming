6ae7cda467a42efbd65eb7c3f6a81d1dfad62266
KimaniSarah <bibokimani@gmailcom>
2023-09-13 03:35:09 -0700

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,8 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, "r+", encoding="utf-8") as f:
         lines = f.readlines()
+        f.seek(0)
         for line in lines:
             f.write(line)
             if search_string in line:
                 f.write(new_string)
+
+        f.truncate()
     return filename
