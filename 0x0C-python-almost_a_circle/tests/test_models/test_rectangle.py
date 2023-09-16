05ecae3616de39bcb342d71df522a6a3b133a477
KimaniSarah <bibokimani@gmailcom>
2023-09-16 03:20:39 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,9 +5,48 @@ from models.rectangle import Rectangle
 
 
 class testRectangle(unittest.TestCase):
-    def Noneis(self):
-        instance =  Rectangle(10, 2)
-        instance2 = Rectangle(10, 2, 0, 0, 12)
+    def test_rect_dimensions(self):
+        rec = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(rec.id, 12)
+        self.assertEqual(rec.width, 10)
+        self.assertEqual(rec.height, 2)
+        self.assertEqual(rec.x, 0)
+        self.assertEqual(rec.y, 0)
 
-        self.assertEqual(instance._Rectangle__nb_objects, 1)
-        self.assertEqual(instance2._Rectangle__nb_objects, 1)
+    def test_width(self):
+        """Test that an exception is raised when width is not an integer or <= 0"""
+        with self.assertRaises(TypeError):
+            Rectangle("1", 4, 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 3)
+
+    def test_height(self):
+        """Test that an exception is raised when height is not an integer or <= 0"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, "4")
+        with self.assertRaises(ValueError):
+            Rectangle(1, -3)
+
+    def test_x(self):
+        """Test that an exception is raised when x is not an integer or < 0"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, 4, "2", 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 5, -3, 7)
+
+    def test_y(self):
+        """Test that an exception is raised when y is not an integer or < 0"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, 4, 2, "0")
+        with self.assertRaises(ValueError):
+            Rectangle(1, 5, 3, -7)
+
+    def test_area(self):
+        rect = Rectangle(3, 8)
+        self.assertEqual(rect.area(), 24)
+
+    def test_str(self):
+        """ Test the string representation"""
+        rect = Rectangle(3, 4, 1, 1, 7)
+        expected_str = "[Rectangle] (7) 1/1 - 3/4"
+        self.assertEqual(str(rect), expected_str)
