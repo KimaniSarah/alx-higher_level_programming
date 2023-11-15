7752756866000ab1d0a673da4db4cbc6b9f2c15b
KimaniSarah <bibokimani@gmailcom>
2023-11-14 19:38:29 -0800

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = "C is fun";
+console.log(firstLine);
+const secondLine = "Python is cool";
+console.log(secondLine);
+const thirdLine = "JavaScript is amazing";
+console.log(thirdLine);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const argAsInt = parseInt(arg);
+if (!isNaN(argAsInt)) {
+  console.log(`My number: ${argAsInt}`);
+} else {
+  console.log("Not a number");
+}
