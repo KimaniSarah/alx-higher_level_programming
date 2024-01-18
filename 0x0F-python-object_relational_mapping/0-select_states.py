eb07b7395918f7fe05a904acd499c4753f3bf562
KimaniSarah <bibokimani@gmailcom>
2024-01-17 20:45:58 -0800

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+"""Listes all states"""
+
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
-if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    MyDb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    myCur = MyDb.cursor()
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    cur.close()
-    db.close()
+    myCur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = myCur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    myCur.close()
+    MyDb.close()
