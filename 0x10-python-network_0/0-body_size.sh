a2f7f725bc73cc14a6e91f055b2694d40a0f8e64
KimaniSarah <bibokimani@gmailcom>
2024-01-26 00:47:58 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-size=$(curl -sI "$url" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-
-echo "$size"
+# displays the size
+curl -s "$1" | wc -c
